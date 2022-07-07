4988ee2952c7497b594f5812ef12554b34da5657
Narvin <narvinp16@gmail.com>
2022-07-07 12:39:40 +0530

    Done

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,44 @@
+// import React from 'react';
+
+// // import { Autocomplete } from '@react-google-maps/api';
+// import { AppBar,Toolbar,Typography,InputBase,Box } from '@mui/material';
+// import SearchTwoToneIcon from '@mui/icons-material/SearchTwoTone';
+
+// import useStyles from './styles'
+
+
+// const Header = () =>{
+
+//     const classes = useStyles();
+
+//     return(
+//        <AppBar position='static'>
+//               <Toolbar className={classes.toolbar}>
+//                      <Typography variant="h5" className={classes.title}>
+//                             Travel Advisor
+//                      </Typography>
+//                      <Box display='flex'>
+//                             <Typography variant="h6" className={classes.title}>
+//                                     Explore New Places
+//                             </Typography>
+//                             <Autocomplete>
+//                                 <div className={classes.search}>
+//                                     <div className={classes.searchIcon}>
+//                                         <SearchTwoToneIcon/>
+//                                     </div>
+//                                     <InputBase placeholder="Search..." classes={{root:classes.inputRoot , input:classes.inputInput}} />
+//                                 </div>
+//                             </Autocomplete>
+//                      </Box>
+//               </Toolbar>
+//        </AppBar>
+            
+
+            
+//     );
+// }
+
+
+
+
+// export default Header;
